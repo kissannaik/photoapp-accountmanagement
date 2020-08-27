ab2796df913e80d4e8b839182fe517df7606971e
Kissan Naik <kissannaik@yahoo.com>
2020-08-27 21:19:52 +0530

    Changes to add dependencies

--- src/main/java/com/kissan/photoappaccountmanagement/ui/controller/AccountController.java
+++ src/main/java/com/kissan/photoappaccountmanagement/ui/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.kissan.photoappaccountmanagement.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/accounts")
 public class AccountController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status")
     public String getStatus(){
-        return "AccountMgmt Working";
+        return "AccountMgmt Working on port "+env.getProperty("local.server.port");
     }
 }
